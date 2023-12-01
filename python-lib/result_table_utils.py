3cc08784f4fe7334c34f3ac6d76acedc741b6f65
Mehdi Hamoumi <mhham@users.noreply.github.com>
2023-12-01 20:09:01 +0100

    Add info about skipped/failed model bumps + logging + only bump old models

--- python-lib/result_table_utils.py
+++ python-lib/result_table_utils.py
@@ -1,14 +1,15 @@
 from dataiku.runnables import Runnable, ResultTable
 
 
-def create_result_table(bumped_xgboost_models):
+def create_result_table(bump_results):
     rt = ResultTable()
 
-    rt.add_column("clf_path", "Model file path", "STRING")
+    rt.add_column("model_path", "Model file path", "STRING")
     rt.add_column("model_origin", "Model origin", "STRING")
     rt.add_column("algorithm", "Algorithm", "STRING")
+    rt.add_column("bump_result", "Bump result", "STRING")
 
-    for model_info in bumped_xgboost_models:
-        rt.add_record(model_info)
+    for bump_result in bump_results:
+        rt.add_record(bump_result)
 
     return rt
--- python-lib/xgboost_bump.py
+++ python-lib/xgboost_bump.py
@@ -7,13 +7,18 @@ import os
 try:
     from dataiku.doctor.utils import model_io
 except Exception as e:
-    # TODO 12.2 or higher ?
-    raise ImportError("Failed to import dataiku.doctor.utils.model_io. Check that your DSS version is higher than 12.2", e.message)
+    raise ImportError("Failed to import dataiku.doctor.utils.model_io. Check that your DSS version is higher than 13.0.0", e.message)
 
 try:
     from dataiku.base.folder_context import build_folder_context
 except Exception as e:
-    raise ImportError("Failed to import dataiku.base.folder_context.build_folder_context. Check that your DSS version is higher than 12.3", e.message)
+    raise ImportError("Failed to import dataiku.base.folder_context.build_folder_context. Check that your DSS version is higher than 13.0.0", e.message)
+
+try:
+    from dataiku.doctor.prediction.dku_xgboost import XGBOOST_BOOSTER_FILENAME
+    from dataiku.doctor.prediction.dku_xgboost import XGBOOST_CLF_ATTRIBUTES_FILENAME
+except Exception as e:
+    raise ImportError("Failed to import from dataiku.doctor.utils.dku_xgboost. Check that your DSS version is higher than 13.0.0", e.message)
 
 
 XGB_PREDICTION_TYPES = ["XGBOOST_CLASSIFICATION", "XGBOOST_REGRESSION"]
@@ -33,19 +38,20 @@ def bump_saved_model(project_id, saved_model_id):
 
     :param str project_id: Project id containing the saved model
     :param str saved_model_id: Saved model id to bump
-    :return List[tuple]: list of (clf_path, model_origin, algorithm) tuples for all bumped xgboost models
+    :return List[tuple]: list of (model_path, model_origin, algorithm, bump_result) tuples for all xgboost models
     """
-    bumped_xgboost_models = []
+    logging.info("Bumping all XGBoost models in saved model: %s > %s", project_id, saved_model_id)
+    all_bump_results = []
     saved_model_folder = os.path.join(DIP_HOME, "saved_models", project_id, saved_model_id)
     for rmodeling_params_path in (
         glob("{}/versions/*/rmodeling_params.json".format(saved_model_folder)) +
         glob("{}/pversions/*/*/rmodeling_params.json".format(saved_model_folder))
     ):
-        bumped_model = _possibly_bump_model(rmodeling_params_path, MODEL_ORIGIN.SAVED_MODEL)
-        if bumped_model is not None:
-            bumped_xgboost_models.append(bumped_model)
+        bump_result = _possibly_bump_model(rmodeling_params_path, MODEL_ORIGIN.SAVED_MODEL)
+        if bump_result is not None:
+            all_bump_results.append(bump_result)
     
-    return bumped_xgboost_models
+    return all_bump_results
 
 
 def bump_analysis_models(project_id, analysis_id):
@@ -53,75 +59,59 @@ def bump_analysis_models(project_id, analysis_id):
 
     :param str project_id: Project id containing the analysis
     :param str analysis_id: Analysis id to bump
-    :return List[tuple]: list of (clf_path, model_origin, algorithm) tuples for all bumped xgboost models
+    :return List[tuple]: list of (model_path, model_origin, algorithm, bump_result) tuples for all xgboost models
     """
-    bumped_xgboost_models = []
+    logging.info("Bumping all XGBoost models in analysis: %s > %s", project_id, analysis_id)
+    all_bump_results = []
     analysis_folder = os.path.join(DIP_HOME, "analysis-data", project_id, analysis_id)
     for rmodeling_params_path in glob("{}/*/sessions/*/*/*/rmodeling_params.json".format(analysis_folder)):
-        bumped_model = _possibly_bump_model(rmodeling_params_path, MODEL_ORIGIN.ANALYSIS)
-        if bumped_model is not None:
-            bumped_xgboost_models.append(bumped_model)
+        bump_result = _possibly_bump_model(rmodeling_params_path, MODEL_ORIGIN.ANALYSIS)
+        if bump_result is not None:
+            all_bump_results.append(bump_result)
      
-    return bumped_xgboost_models
+    return all_bump_results
 
 
 def bump_all_analyses_and_saved_models(project_id):
     """Bump all XGBoost models in the project
 
     :param str project_id: Project id containing the analysis
-    :return List[tuple]: list of (clf_path, model_origin, algorithm) tuples for all bumped xgboost models
+    :return List[tuple]: list of (clf_path, model_origin, algorithm, bump_result) tuples for all xgboost models
     """
-    bumped_xgboost_models = []
+    logging.info("Bumping all XGBoost models in project: %s", project_id)
+    all_bump_results = []
     saved_models_folder = os.path.join(DIP_HOME, "saved_models", project_id)
     for rmodeling_params_path in (
         glob("{}/*/versions/*/rmodeling_params.json".format(saved_models_folder)) +
         glob("{}/*/pversions/*/*/rmodeling_params.json".format(saved_models_folder))
     ):
-        bumped_model = _possibly_bump_model(rmodeling_params_path, MODEL_ORIGIN.SAVED_MODEL)
-        if bumped_model is not None:
-            bumped_xgboost_models.append(bumped_model)
+        bump_result = _possibly_bump_model(rmodeling_params_path, MODEL_ORIGIN.SAVED_MODEL)
+        if bump_result is not None:
+            all_bump_results.append(bump_result)
  
     analysis_data_folder = os.path.join(DIP_HOME, "analysis-data", project_id)
     for rmodeling_params_path in glob("{}/*/*/sessions/*/*/*/rmodeling_params.json".format(analysis_data_folder)):
-        bumped_model = _possibly_bump_model(rmodeling_params_path, MODEL_ORIGIN.ANALYSIS)
-        if bumped_model is not None:
-            bumped_xgboost_models.append(bumped_model)
+        bump_result = _possibly_bump_model(rmodeling_params_path, MODEL_ORIGIN.ANALYSIS)
+        if bump_result is not None:
+            all_bump_results.append(bump_result)
      
-    return bumped_xgboost_models
-
-
-def _bump_xgboost_model(model_folder):
-    """
-    Bump XGBoost model in model folder from 'clf.pkl' format to new backwards compatible format
-    with 'xgboost_clf_attributes.json' (Classifier attributes needed for instantiation) and 
-    'xgboost_booster.bin' (Booster serialized in backwards compatible format) files.
-
-    :param str model_folder: path to model folder container 
-    :return bool: Whether the model was successfully bumped
-    """
-    logging.info("Bumping XGBoost model in: %s", model_folder)
-    try:
-        folder_context = build_folder_context(model_folder)
-        clf = model_io.load_model_from_folder(folder_context)
-        model_io.dump_model_to_folder(folder_context, clf)
-    except:
-        logging.warn("Failed to bump XGBoost model: ", exc_info=True)
-        return False
-    
-    return True
+    return all_bump_results
 
 
 def _possibly_bump_model(rmodeling_params_path, model_origin):
     """
     Check:
     - if model is an XGBoost one
-    - if it has an old 'clf.pkl' format file 
-    and if so try to bump it into the new backwards compatible format.
+    - if it has not already been bumped to the new backwards compatible format
+
+    Then bump XGBoost model in model folder from 'clf.pkl' format to new backwards compatible format
+    with 'xgboost_clf_attributes.json' (Classifier attributes needed for instantiation) and 
+    'xgboost_booster.bin' (Booster serialized in backwards compatible format) files.
 
     :param str rmodeling_params_path: path to the rmodeling_params.json file of the model
     :param MODEL_ORIGIN model_origin: Whether model is a Saved Model or an Analysis model
-    :return tuple | None: (clf_path, model_origin, algorithm) tuple if successfully bumped,
-                          None otherwise
+    :return tuple | None: (model_path, model_origin, algorithm, bump_result) tuple if it is an
+                          XGBoost model, None otherwise
     """
     with open(rmodeling_params_path) as f:
         modeling_params = json.load(f)
@@ -129,11 +119,24 @@ def _possibly_bump_model(rmodeling_params_path, model_origin):
 
     if algorithm in XGB_PREDICTION_TYPES:
         model_folder = os.path.dirname(rmodeling_params_path)
+
+        # If model has already been bumped, skip
+        xgb_clf_attributes_path = os.path.join(model_folder, XGBOOST_CLF_ATTRIBUTES_FILENAME)
+        xgb_booster_path = os.path.join(model_folder, XGBOOST_BOOSTER_FILENAME)
+        if os.path.exists(xgb_clf_attributes_path) and os.path.exists(xgb_booster_path):
+            logging.warn("Bumped Xgboost files already exist: '%s', '%s'", xgb_booster_path, xgb_clf_attributes_path)
+            return (xgb_clf_attributes_path, model_origin, algorithm, "SKIPPED")
+
+        # Try bumping model from clf.pkl
         clf_path = os.path.join(model_folder, "clf.pkl")
-        if os.path.exists(clf_path):
-            success = _bump_xgboost_model(model_folder)
-            # success = True
-            if success:
-                return (clf_path, model_origin, algorithm)
-    
+        try:
+            folder_context = build_folder_context(model_folder)
+            clf = model_io.load_model_from_folder(folder_context)
+            model_io.dump_model_to_folder(folder_context, clf)
+            logging.info("Successfully bumped XGBoost model: %s", clf_path)
+            return (clf_path, model_origin, algorithm, "SUCCESS")
+        except:
+            logging.warn("Failed to bump XGBoost model: '%s'", clf_path, exc_info=True)
+            return (clf_path, model_origin, algorithm, "FAIL")
+
     return None
